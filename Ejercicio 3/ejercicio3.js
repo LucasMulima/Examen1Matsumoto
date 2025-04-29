275da64eda17d82fd7481b6d2b9d7f4a1112b004
lucas <20221543@aloe.ulima.edu.pe>
2025-04-28 21:49:03 -0500

    examen

--- Ejercicio 3/ejercicio3.js
+++ Ejercicio 3/ejercicio3.js
@@ -1,5 +1,11 @@
+let guardar = function(){
+    let nombre = document.getElementById("nom")
+    let edad = document.getElementById("edad")
+    
+}
+
 let main = function(){
     let boton = document.getElementById("button")
-    boton
+    boton.addEventListener("click",guardar)
 }
 main()
